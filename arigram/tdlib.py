52e353e9aa49a5ba68e23d4aabfa9515b045585b
Ari Archer <truncateddinosour@gmail.com>
2021-12-02 19:59:19 +0200

    fixed imports

--- arigram/tdlib.py
+++ arigram/tdlib.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from typing import Any, Dict, List, Optional, Union, Tuple
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 from telegram.client import AsyncResult, Telegram
 
